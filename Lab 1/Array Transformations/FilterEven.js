d572cd48883c10bc2ce2b37cad0de4c4e127a0f3
Ebenezer Antwi <ebenezer.antwi@amalitech.com>
2024-11-06 14:44:31 +0000

    Remove folders from main

--- Lab 1/Array Transformations/FilterEven.js
+++
@@ -1,8 +0,0 @@
-// Filter even numbers from an array
-function filterEven(arr) {
-  // Return a new array with even numbers
-  return arr.filter((number) => number % 2 === 0);
-}
-
-// Log even numbers from the array
-console.log(filterEven([1, 2, 3, 4, 5, 6, 25, 30]));
